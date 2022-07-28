5e04dc78c147bf73aac85c60d173649fe3033d7f
LunarBirdMYT <pav.mgk50@mail.ru>
2022-07-28 16:30:42 +0300

    Добавлены тестовые данные в БД, создана модельв  админке

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def index(request):
     template = 'posts/index.html'
-    title = 'Это главная страница проекта Yatube'
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
     context = {
-        'title': title
+        'posts': posts,
     }
     return render(request, template, context)
 
