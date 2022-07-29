96673a5475a3a22afcbb6542e5991f362c20d718
LunarBirdMYT <pav.mgk50@mail.ru>
2022-07-29 09:20:07 +0300

    Исправлен базовый шаблон, создана новая модель

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,20 +1,23 @@
 from django.contrib import admin
-from .models import Post
+from .models import Group, Post
 # Register your models here.
 
 
 class PostAdmin(admin.ModelAdmin):
     # Перечисляем поля, которые должны отображаться в админке
-    list_display = ['pk', 'text', 'pub_date', 'author']
+    list_display = ['pk', 'text', 'pub_date', 'author', 'group']
+    # Свойство для изменения группы поста
+    list_editable = ('group',)
     # Добавляем интерфейс для поиска по тексту постов
     search_fields = ('text',)
     # Добавляем возможность фильтрации по дате
     list_filter = ('pub_date',)
     # Это свойство сработает для всех колонок: где пусто — там будет эта строка 
-    empty_value_display = '-пусто-' 
+    empty_value_display = '-пусто-'
+
 
 # При регистрации модели Post источником конфигурации для неё назначаем
 # класс PostAdmin
 admin.site.register(Post, PostAdmin)
-
+admin.site.register(Group)
 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-from .models import Post
+from .models import Group, Post
 
 # Create your views here.
 def index(request):
@@ -16,10 +16,18 @@ def index(request):
     return render(request, template, context)
 
 def group_posts(request, slug):
-    # return HttpResponse(f'Страничка с группой {slug}...пошла жара!')
     template = 'posts/group_list.html'
-    title = 'Здесь будет информация о группах проекта Yatube'
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': title
+        'group': group,
+        'posts': posts,
     }
     return render(request, template, context)
