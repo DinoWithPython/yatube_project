5982d41f202f40c96dfda82b83e409e7960fad73
Павлов-Теремок Дмитрий <pav.mgk50@mail.ru>
2022-08-07 21:35:37 +0300

    Необязательные редиректы

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -140,3 +140,10 @@ LOGIN_URL = 'users:login'
 LOGIN_REDIRECT_URL = 'posts:index'
 
 # LOGOUT_REDIRECT_URL = 'posts:index'
+
+
+# Email action
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
