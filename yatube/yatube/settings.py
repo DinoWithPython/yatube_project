6600ccb23baaae8a9dc4eade2721657518ff14b0
Павлов-Теремок Дмитрий <pav.mgk50@mail.ru>
2022-08-07 15:53:28 +0300

    Добавлен шаблон авторизации

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -131,3 +131,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+
+# Redirecting user on page
+
+LOGIN_URL = 'users:login'
+
+LOGIN_REDIRECT_URL = 'posts:index'
+
+# LOGOUT_REDIRECT_URL = 'posts:index'
