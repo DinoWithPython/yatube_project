1bb3b767ccb6d72d86ebd195939512e681d8bdec
LunarBirdMYT <pav.mgk50@mail.ru>
2022-08-10 14:35:40 +0300

    Добавлен пагинатор на главную и страницы групп

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.conf import settings
 from django.shortcuts import get_object_or_404, render
 # from django.contrib.auth.decorators import login_required
@@ -8,10 +9,15 @@ from .models import Group, Post
 # @login_required
 def index(request):
     template = 'posts/index.html'
-    posts = Post.objects.prefetch_related(
-        'author')[:settings.COUNT_OF_VISIBLE_POSTS]
+    posts = Post.objects.prefetch_related('author')
+
+    paginator = Paginator(posts, settings.COUNT_OF_VISIBLE_POSTS)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'posts': posts
+        'page_obj': page_obj,
     }
     return render(request, template, context)
 
@@ -19,10 +25,15 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.prefetch_related('author')[
-        :settings.COUNT_OF_VISIBLE_POSTS]
+    posts = group.posts.prefetch_related('author')
+
+    paginator = Paginator(posts, settings.COUNT_OF_VISIBLE_POSTS)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
     context = {
         'group': group,
-        'posts': posts
+        'page_obj': page_obj
     }
     return render(request, template, context)
