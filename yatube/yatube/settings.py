82867a7e716856694445100b795eb34ebc7737dd
LunarBirdMYT <pav.mgk50@mail.ru>
2022-09-02 12:52:27 +0300

    Добавлены кастомные шаблоны для 403csrf и 404.

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -155,3 +155,6 @@ LOGIN_REDIRECT_URL = 'posts:index'
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+
+# csrf константа
+CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
