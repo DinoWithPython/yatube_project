879cd7c9594ab28fe17e57838eecf8a8f4a8b899
LunarBirdMYT <pav.mgk50@mail.ru>
2022-09-02 16:04:32 +0300

    Добавлена проверка на наличие изображения в тест формы.

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -1,9 +1,17 @@
-from django.test import Client, TestCase
+import shutil
+import tempfile
+
+from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.test import Client, TestCase, override_settings
 from django.urls import reverse
 
 from posts.models import Group, Post, User
 
+TEMP_MEDIA_ROOT = tempfile.mkdtemp(dir=settings.BASE_DIR)
 
+
+@override_settings(MEDIA_ROOT=TEMP_MEDIA_ROOT)
 class PostsCreateFormTest(TestCase):
     @classmethod
     def setUpClass(cls):
@@ -26,13 +34,32 @@ class PostsCreateFormTest(TestCase):
         self.authorized_client = Client()
         self.authorized_client.force_login(self.second_user)
 
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        shutil.rmtree(TEMP_MEDIA_ROOT, ignore_errors=True)
+
     def test_post_add_database(self):
         """Проверка записи поста в БД."""
         posts_count = Post.objects.count()
+        small_gif = (            
+             b'\x47\x49\x46\x38\x39\x61\x02\x00'
+             b'\x01\x00\x80\x00\x00\x00\x00\x00'
+             b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+             b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+             b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+             b'\x0A\x00\x3B'
+        )
+        uploaded = SimpleUploadedFile(
+            name='small.gif',
+            content=small_gif,
+            content_type='image/gif'
+        )
         form_data = {
             'author': self.second_user,
             'group': self.group.id,
-            'text': 'Тестовый пост'
+            'text': 'Тестовый пост',
+            'image': uploaded
         }
         self.authorized_client.post(
             reverse('posts:post_create'),
@@ -43,6 +70,7 @@ class PostsCreateFormTest(TestCase):
                          self.second_user.username)
         self.assertEqual(latest_post.group.id, self.group.id)
         self.assertEqual(latest_post.text, form_data['text'])
+        self.assertTrue(latest_post.image)
         self.assertEqual(Post.objects.count(), posts_count + 1)
 
     def test_edit_form_change_record_in_database(self):
